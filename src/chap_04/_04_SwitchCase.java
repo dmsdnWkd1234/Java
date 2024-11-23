464490ffe297b2f2744114f59b92e3d312c7d996
kimeunwoo <gumman0620@gmail.com>
2024-11-23 18:18:05 +0900

    2024.11.16

---
+++ src/chap_04/_04_SwitchCase.java
@@ -0,0 +1,13 @@
+package chap_04;
+
+public class _04_SwitchCase {
+    public static void main(String[] args) {
+        //Switch Case
+        // 1등 : 전액
+        // 2등 : 반액
+        // 3등 : 반액
+        // 그 외 : 장학금 x
+
+
+    }
+}
